e55497307664525334ca967ebc3d407e2454e95c
AndrewK225 <andrew.koe315@gmail.com>
2015-03-28 18:28:17 -0400

    Added Sign In query

--- DBUtils.java
+++ DBUtils.java
@@ -23,16 +23,78 @@ public final class DBUtils {
             Connection conn = DriverManager.getConnection(url,p);
             
             //do the sql stuffs
-            Statement st = conn.createStatement();
-            st.executeUpdate("INSERT INTO Accounts (Username, Password, Email, Wins, Losses) VALUE ('"+user+"','"+hashed_pass+"','"+email+"','"+wins+"','"+losses+"')");
+            
+            String insert_statement = "INSERT INTO Accounts (Username, Password, Email, Wins, Losses) VALUE(?,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(insert_statement);
+            ps.setString(1,user);
+            ps.setString(2, hashed_pass);
+            ps.setString(3, email);
+            ps.setInt(4, wins);
+            ps.setInt(5, losses);
+            
+            ps.executeUpdate();
             conn.close();
             
         } catch(Exception e) {
         	System.err.println(e);
         }
-
     }
 
+    /*Query database for user account.
+     * returns 0 for success
+     * return 1 for username error
+     * return 2 for password error
+     * return 3 for exception
+     * */
+    public static byte signIn(String user, String clear_pass) {
+    	try {
+    		String stored_pass = null; 
+    		String entered_pass = null; //hashed version of the user cleartxt password 
+    		
+    		// access the database
+    		Properties p = new Properties();
+    		p.put("user", "andrew");
+    		p.put("password", "password");
+    		String url= "jdbc:mysql://127.0.0.1/set_game";
+    		Connection conn = DriverManager.getConnection(url,p);
+    		
+    		//query the database
+    		String query_statement = "SELECT Password FROM Accounts WHERE BINARY Username = ?";
+    		PreparedStatement ps = conn.prepareStatement(query_statement);
+    		ps.setString(1, user);
+    		ResultSet rs = ps.executeQuery();
+    		
+    		
+    		//get stored password
+    		if(rs.next()) {
+    			stored_pass = rs.getString("Password");
+    			conn.close();
+    		} else {  //username not found
+    			conn.close();
+    			return 1;
+    		}
+
+    		
+    		//hash the entered password and see if it matches value in DB
+    		entered_pass = hash(clear_pass);
+    		if(entered_pass.equals(stored_pass)) {
+    			return 0;
+    		}
+    		else { //password doesn't match
+    			return 2;
+    		}
+    		
+    	} catch(Exception e) {
+    		System.err.println(e);
+    		return 3;
+    	}
+    	
+    	
+    	
+    }
+    
+    
+    
     //use an MD5 hash 
     private static String hash(String cleartxt) throws NoSuchAlgorithmException {
 
@@ -44,9 +106,12 @@ public final class DBUtils {
             sb.append(String.format("%02x", b & 0xff));
         }
         return sb.toString();
-
-
-
-
     }
+    
+    
+    
+    
+    
+    
+    
 }
