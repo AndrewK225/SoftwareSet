2e73295d9d9879717effb4943ff839961492c715
AndrewK225 <andrew.koe315@gmail.com>
2015-03-27 22:38:36 -0400

    Update Utils_test.java

--- Utils_test.java
+++ Utils_test.java
@@ -3,7 +3,7 @@ public class Utils_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DBUtils.signup("Andrew", "pass","a@gmail.com");
+		DBUtils.signUp("Andrew", "pass","a@gmail.com");
 
 	}
 
