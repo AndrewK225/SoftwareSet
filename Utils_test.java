b045cc6c94ca502cf3c5180da253dc6b86ab9a76
AndrewK225 <andrew.koe315@gmail.com>
2015-03-28 18:31:22 -0400

    usage of signIn

--- Utils_test.java
+++ Utils_test.java
@@ -3,8 +3,9 @@ public class Utils_test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DBUtils.signUp("Andrew", "pass","a@gmail.com");
-
+		DBUtils.signUp("bob", "123", "t@gmail.com");
+		byte a = DBUtils.signIn("Andrew", "123");
+		System.out.println(a);
 	}
 
 }
