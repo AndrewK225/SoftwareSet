5c825aa67070d8e78bc00b20603470df80249aa8
Patel Miraj <mirage.patel@gmail.com>
2015-04-29 11:48:52 -0400

    Changed the Card's print function to return a string that describes the card using 4 digit number

--- Card.java
+++ Card.java
@@ -10,7 +10,45 @@ public class Card{
  		this.number = number;
  		this.shade = shade;
  	}
- 	public void printCard(){
- 		System.out.println("Color = " + color + " Shape = " + shape + " Number = " + number + " Shade = " + shade);
- 	}
+ 	public String printCard(){
+ 		String retStr = "";
+        System.out.println("Color = " + color + " Shape = " + shape + " Number = " + number + " Shade = " + shade);
+
+    if ("purple".equals(color))
+        retStr += "0";
+    else if ("green".equals(color))
+        retStr += "1";
+    else if ("red".equals(color))
+        retStr += "2";
+    else
+        retStr += "-COLORUNKNOWN-";
+
+    if ("oval".equals(shape))
+        retStr += "0";
+    else if ("diamond".equals(shape))
+        retStr += "1";
+    else if ("squiggle".equals(shape))
+        retStr += "2";
+    else
+        retStr += "-SHAPEUNKNOWN-";
+
+    if ("1".equals(number))
+        retStr += "0";
+    else if ("2".equals(number))
+        retStr += "1";
+    else if ("3".equals(number))
+        retStr += "2";
+    else
+        retStr += "-NUMBERUNKNOWN-";
+
+    if ("clear".equals(shade))
+        retStr += "0";
+    else if ("striped".equals(shade))
+        retStr += "1";
+    else if ("solid".equals(shade))
+        retStr += "2";
+    else
+        retStr += "-SHADEUNKNOWN-";
+
+    return retStr;
  } 
