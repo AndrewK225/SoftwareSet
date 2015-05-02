9f446848a58d8029c5590537e524a925c5f4d789
student <student@ice06.ee.cooper.edu>
2015-05-02 19:28:32 -0400

    fixed up some stuff

--- Game.java
+++ Game.java
@@ -1,53 +1,65 @@
 import java.util.*;
 
 public class Game{
-		Deck deck;
-		Board board;
-		HashMap players = new HashMap();
-		int gameNum;
-		 public Game(int num){
-			gameNum = num;
-	        deck = new Deck();
-	        // shuffle after this actually works - will see cards in order then, theoretically
-	         deck.shuffle();
-	        board = new Board(deck);
-		
-	 //int score = 0; //initialize the player's score to 0
-	 Scanner user_input = new Scanner( System.in );
-     while(deck.num_cards > 2 || (board.is_set())){ //while the deck still has at least 3 cards
-		System.out.println("start of while loop. checking for sets. deck size: " + deck.num_cards + " board size: " + board.num_cards);
-		if ((!board.is_set())&&(deck.num_cards > 2)){ //if there are no sets on the board or if the board has under 12 cards, 
-			//add 3 cards from the deck to the board
-			System.out.println("Adding cards. Deck size: " + deck.num_cards);
+	Deck deck;
+	Board board;
+	Timer timer;
+	HashMap <String, Player> players;
+	boolean lock_set = false;
+	int gameNum;
+	int[] inputs = {-1 -1 -1};
+	int num_inputs = 0;
+	Player declare_set_player;
+	public void addPlayer(String name, Player player){
+		players.put(name,player);
+	}
+	public void removePlayer(String name, Player player){
+		players.remove(name);
+	}
+	public void declare_set(String name){
+		lock_set = true;
+		declare_set_player = players.get(name);
+	}
+	public void enter_card(int index){
+		inputs[num_inputs] = index;
+		num_inputs++;
+	}
+	public void release_lock(){
+		lock_set = false;
+	}
+	public boolean is_over(){
+		return ((deck.num_cards < 3) && (!board.is_set())); 
+	}
+	public void loop(){
+		//this is the function that keeps being called as the game is running
+		board.displayBoard();
+		while ((!board.is_set())&&(deck.num_cards > 2)&&(!lock_set)){ 
+			//System.out.println("Adding cards. Deck size: " + deck.num_cards);
 			board.addTriplet(deck);
-		}else{ //if there is at least one set on the board
-			board.displayBoard(); //display the board
-			int index1 = user_input.nextInt( ); //get three inputs from user
-			int index2 = user_input.nextInt( );
-			int index3 = user_input.nextInt( );
-			System.out.println("user input provided. checking if valid set"); 
-			if (board.is_set(index1,index2,index3)){ 
-				if ((board.num_cards) > 12 || (deck.num_cards < 3)){
-					System.out.println("removing 3 cards");
-					board.removeTriplet(index1,index2,index3);
-				}else if (board.num_cards == 12){
-					System.out.println("replacing 3 cards");
-					board.addTriplet(deck,index1,index2,index3);
-				}
-				System.out.println("You found a set!");
-				//score++;
-			}else{
-				System.out.println("Not a set!");
-			}
 		}
-     }
-    user_input.close();
-		 }
+	}
+	public void process_input(String name,int input1,int input2,int input3){
+		if (board.is_set(input1,input2,input3)){ 
+			if ((board.num_cards) > 12 || (deck.num_cards < 3)){
+				//System.out.println("removing 3 cards");
+				board.removeTriplet(input1,input2,input3);
+			}else if (board.num_cards == 12){
+				//System.out.println("replacing 3 cards");
+				board.addTriplet(deck,input1,input2,input3);
+			}
+			System.out.println("You found a set!");
+			players.get(name).score++;
+		}else{
+			System.out.println("Not a set!");
+		}
+	}
+	
+	public Game(int num){
+		gameNum = num;
+		deck = new Deck();
+		deck.shuffle();
+		board = new Board(deck);
+	}
 }
-   /*
-    * while game is still going:
-    * 	if the deck has >3 cards and board < 12 cards:
-    * 		add 3 cards 
-  */
 
 
